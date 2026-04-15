7ab5976a533a2cef64d9eeb51e1cb3a3dc36afef
Wayne <jaweplanos22@gmail.com>
2026-04-16 00:50:20 +0800

    changed create account route to register page

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -103,7 +103,7 @@ export default function Login() {
                     <p className="text-center text-sm text-gray-500 mt-6">
                         New to tasteCebu?{' '}
                         <span
-                            onClick={() => navigate('/signup')}
+                            onClick={() => navigate('/Register')}
                             className="text-orange-500 font-semibold hover:underline cursor-pointer"
                         >
                             Create an Account
