a7dc02ff01cc115454609d81c8b6614d1f746eff
Mkoo-kjl <kristofferlayos78@gmail.com>
2026-05-11 13:39:10 +0800

    Setting up for deployment

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: [process.env.CLIENT_URL, 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'].filter(Boolean),
   credentials: true,
 }));
 app.use(express.json({ limit: '50mb' }));
