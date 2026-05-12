38451e68d56c05c266afa62af8be867b4a68d90f
Mkoo-kjl <kristofferlayos78@gmail.com>
2026-05-12 18:35:17 +0800

    fix backend #6

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
+  password: process.env.DB_PASS || 'BYmiBRykYVMLhmgUTKqJKvgaAZzIKQty',
   database: process.env.DB_NAME || 'tastecebu',
   waitForConnections: true,
   connectionLimit: 10,
